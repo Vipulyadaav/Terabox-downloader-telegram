fe5c66ab966fd32eddba1819eab859a7cdaafc88
Vipulyadaav <162639553+Vipulyadaav@users.noreply.github.com>
2024-03-08 03:43:36 +0530

    utils.js

--- utils.js
+++ utils.js
@@ -1,7 +1,12 @@
+const axios = require('axios');
+
 const sendFile = async (item, ctx) => {
   if (item) {
     try {
-      await ctx.replyWithDocument(item);
+      const response = await axios.get(item, { responseType: 'arraybuffer' });
+      const fileBuffer = Buffer.from(response.data);
+
+      await ctx.replyWithDocument({ source: fileBuffer, filename: 'your_filename.ext' });
     } catch (e) {
       ctx.replyWithMarkdown(
         `⚠️ ${e.message}\n\n👉 Try manually downloading from [here](${item})\n\n👉 *Maybe This File Is Too Large Or Cannot Accessible From Terabox*`,
